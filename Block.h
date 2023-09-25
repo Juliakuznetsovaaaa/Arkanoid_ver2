1768df481f14bf36733c86518fc1ddd43a66d654
Juliakuznetsovaaaa <99537178+Juliakuznetsovaaaa@users.noreply.github.com>
2023-09-25 17:15:21 +0300

    Update Block.h

--- Block.h
+++ Block.h
@@ -12,13 +12,10 @@ protected:
 public:
 	Block(float blockHeight, float blockWidth, float posX, float posY, int blockHealth, sf::Color blockcolor);
 	~Block(void) {}
-	sf::Color getColorForHealth(int health);
-	bool processBlock(Block& block);
-	void setHealth(int new_health);
+	void updateHealth(int new_health);
 	float getHeight();
 	float getWidth();
 	float getPosX();
 	float getPosY();
-	int getHealth();
     sf:: Color getColor();
 };
